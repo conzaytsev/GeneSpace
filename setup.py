ebaf4b24682fb25f4780c930c08f3b7080e867a5
conzaytsev <113913648+conzaytsev@users.noreply.github.com>
2025-10-17 11:28:58 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name="genespace", 
+    version='0.1.0',
+    author="Konstantin Zaytsev",
+    url='https://github.com/conzaytsev/GeneSpace',
+    description='Gene Space',
+    long_description='Python module for analysis of genomic sequences as points a multidimensional Gene Space.',
+    packages=['genespace'],
+    install_requires=['sklearn', 'numpy'],
+    python_requires='>3.9',
+    classifiers= [
+        'License :: OSI Approved :: GPL-3.0 License',
+        'Programming Language :: Python :: 3'
+    ]
+)
