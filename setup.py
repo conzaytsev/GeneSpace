21c0f85a04786369f28d82c25a8cdd66eb7ce969
conzaytsev <113913648+conzaytsev@users.noreply.github.com>
2025-10-27 16:00:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Gene Space',
     long_description='Python module for analysis of genomic sequences as points a multidimensional Gene Space.',
     packages=['genespace'],
-    install_requires=['sklearn', 'numpy'],
+    install_requires=['scikit-learn', 'numpy'],
     python_requires='>3.9',
     classifiers= [
         'License :: OSI Approved :: GPL-3.0 License',
