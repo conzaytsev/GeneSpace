7384f7a190c3ae03d320c2ebac6f14bee6bc8a03
conzaytsev <113913648+conzaytsev@users.noreply.github.com>
2025-10-27 18:09:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=['scikit-learn', 'numpy'],
     python_requires='>3.9',
     classifiers= [
-        'License :: OSI Approved :: GPL-3.0 License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3'
     ]
 )
