3fb1edad7ea34ae9459015bdc41725d8836daf96
conzaytsev <113913648+conzaytsev@users.noreply.github.com>
2025-10-27 18:10:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="genespace", 
-    version='0.1.1',
+    version='0.1.0',
     author="Konstantin Zaytsev",
     url='https://github.com/conzaytsev/GeneSpace',
     description='Gene Space',
